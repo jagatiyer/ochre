1d82a59ad0614bbf701daed66f490eeb89ac60c8
akil <akil@everycrave.me>
2025-12-19 11:28:42 +0530

    Admin(blog): inject CKEditor form media via changeform_view (scoped to BlogPost)

--- blog/admin.py
+++ blog/admin.py
@@ -19,14 +19,30 @@ class BlogPostForm(forms.ModelForm):
 class BlogPostAdmin(admin.ModelAdmin):
     form = BlogPostForm
 
-    def get_form(self, request, obj=None, **kwargs):
-        form = super().get_form(request, obj, **kwargs)
+    def changeform_view(self, request, object_id=None, form_url='', extra_context=None):
+        """Inject the BlogPost form widget media into the change form view context.
 
-        # 🔥 FORCE widget media injection
-        if hasattr(form, "media"):
-            self.media = self.media + form.media
+        This ensures CKEditor JS/CSS for the BlogPost `content` field are present
+        even if admin templates or other admin customizations would otherwise
+        omit `{{ media }}`. The injection is local to BlogPostAdmin only.
+        """
+        # Obtain the form class and instantiate it to access its media
+        form_class = self.get_form(request, None)
+        try:
+            form_instance = form_class()
+        except Exception:
+            form_instance = None
 
-        return form
+        # Prepare extra_context and inject media from the form if available
+        extra_context = {} if extra_context is None else dict(extra_context)
+        if form_instance is not None and hasattr(form_instance, 'media'):
+            existing_media = extra_context.get('media')
+            if existing_media:
+                extra_context['media'] = existing_media + form_instance.media
+            else:
+                extra_context['media'] = form_instance.media
+
+        return super().changeform_view(request, object_id, form_url, extra_context=extra_context)
 
     fields = (
         "title",
