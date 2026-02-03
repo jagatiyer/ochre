c04c6c67fe1d1d9ff68b37fc58621429227e08d0
akil <akil@everycrave.me>
2026-02-03 07:04:12 +0530

    admin(static-pages): enable CKEditor for content field

--- pages/admin.py
+++ pages/admin.py
@@ -1,9 +1,24 @@
 from django.contrib import admin
+from django import forms
+from django_ckeditor_5.widgets import CKEditor5Widget
+
 from .models import StaticPage
+
+
+class StaticPageForm(forms.ModelForm):
+    content = forms.CharField(
+        widget=CKEditor5Widget(config_name="blog"),
+        required=False
+    )
+
+    class Meta:
+        model = StaticPage
+        fields = "__all__"
 
 
 @admin.register(StaticPage)
 class StaticPageAdmin(admin.ModelAdmin):
+    form = StaticPageForm
     list_display = ("title", "slug", "show_in_footer", "footer_order", "is_published")
     list_filter = ("show_in_footer", "is_published")
     search_fields = ("title", "slug", "content")
